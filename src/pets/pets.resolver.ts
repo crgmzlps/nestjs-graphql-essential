93812680a3c5ad25af4a4bd3c4b40cf6b7ae867b
Carlo Roberto <crgmzlps@gmail.com>
2021-07-21 21:39:14 -0300

    first query

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,4 +1,12 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Pet } from './models/entities/pet.entity';
+import { PetsService } from './pets.service';
 
-@Resolver()
-export class PetsResolver {}
+@Resolver((of) => Pet)
+export class PetsResolver {
+  constructor(private readonly petsService: PetsService) {}
+  @Query((returns) => [Pet])
+  async pets(): Promise<Pet[]> {
+    return await this.petsService.findAll();
+  }
+}
