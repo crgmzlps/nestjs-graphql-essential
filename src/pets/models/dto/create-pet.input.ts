44d781960cb5e4d75c0ea9528250878115cae7ec
Carlo Roberto <crgmzlps@gmail.com>
2021-07-22 21:29:44 -0300

    add validations

--- src/pets/models/dto/create-pet.input.ts
+++ src/pets/models/dto/create-pet.input.ts
@@ -1,8 +1,11 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, Length } from 'class-validator';
 
 @InputType()
 export class CreatePetInput {
   @Field()
+  @IsNotEmpty()
+  @Length(3, 50)
   name: string;
 
   @Field({ nullable: true })
