ac52261e2a30091dca99ee75e83b928c26ac5d2a
Carlo Roberto <crgmzlps@gmail.com>
2021-07-22 21:12:02 -0300

    create pet & input type

---
+++ src/pets/models/dto/create-pet.input.ts
@@ -0,0 +1,10 @@
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class CreatePetInput {
+  @Field()
+  name: string;
+
+  @Field({ nullable: true })
+  type?: string;
+}
--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreatePetInput } from './models/dto/create-pet.input';
 import { Pet } from './models/entities/pet.entity';
 
 @Injectable()
@@ -10,5 +11,9 @@ export class PetsService {
   ) {}
   async findAll(): Promise<Pet[]> {
     return this.petsRepository.find();
+  }
+  async createPet(createPetInput: CreatePetInput): Promise<Pet> {
+    const newPet = this.petsRepository.create(createPetInput);
+    return this.petsRepository.save(newPet);
   }
 }
